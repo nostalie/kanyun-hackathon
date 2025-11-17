a34f7ccc1a5d3bde1faedee880a7eb36347569de
jindingwen <jindingwen@kanyun.com>
2025-11-17 18:47:45 +0800

    FEAT: 测试js

--- init.sh
+++ init.sh
@@ -3,19 +3,19 @@
 # Player Agent 入口脚本
 # 根据环境变量或参数选择运行 JavaScript 或 Python 版本
 
-echo "Starting Python Player Agent..."
+# echo "Starting Python Player Agent..."
 
-# 激活虚拟环境
-echo "Activating virtual environment..."
-python3 -m venv venv
-source venv/bin/activate
+# # 激活虚拟环境
+# echo "Activating virtual environment..."
+# python3 -m venv venv
+# source venv/bin/activate
 
-pip install -r requirements.txt --quiet --disable-pip-version-check
+# pip install -r requirements.txt --quiet --disable-pip-version-check
 
-# 使用虚拟环境中的 Python 运行脚本
-cd src/python
-venv/bin/activate main.py
+# # 使用虚拟环境中的 Python 运行脚本
+# cd src/python
+# venv/bin/activate main.py
 
-# echo "Starting JavaScript Player Agent..."
-# npm install
-# node src/js/index.js
+echo "Starting JavaScript Player Agent..."
+npm install
+node src/js/index.js
