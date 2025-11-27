bd3c109cca32974f23c99d88a973c4fbce478398
zhangjianbj04 <zhangjianbj04@fenbi.com>
2025-11-27 20:01:58 +0800

    demo

---
+++ src/werewolf-player/Demo.py
@@ -0,0 +1,16 @@
+"""
+多行注释
+1
+2
+3
+"""
+
+# 单行注释
+PLAYER_INDEX = 2
+print("Hello, Python!")
+print(f"玩家位置: {PLAYER_INDEX} 号")  # f-string 格式化
+
+is_video = True
+
+players = [1, 2, 3]
+print(players)
